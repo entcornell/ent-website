6b9dcee54a1030d91d25f925184a906418400407
Samantha Vaca <samanthavaca@Samanthas-MacBook-Air.local>
2025-09-07 13:11:58 -0400

    Updated timeline with Applications Open date

--- src/components/Recruitment.js
+++ src/components/Recruitment.js
@@ -25,6 +25,7 @@ const eventsData = [
   { when: "September", title: "PFC Fair", blurb: "You can find us tabling at this event. Drop by to learn more about ENT. Stay tuned to find out what day it will be taking place.", cta: "See Details", to: "https://www.instagram.com/p/DN4N2qBEcTS/?igsh=ZXQ4ZmVjb3RnM3Bn" },
   { when: "09/04/25", title: "E-Ship Kickoff", blurb: "Stop by the entrepreneurship building and meet the community! You can also come chat to our fellow members to learn more about ENT.", cta: "See Details", to: "https://eship.cornell.edu/all-events/entrepreneurship-kickoff/" },
   { when: "09/10/25", title: "Info Session #1", blurb: "Come to this information ENT event to learn more about what we have to offer!", cta: "RSVP", to: "https://forms.gle/Wd1pRcVHm1RfSVUo9"},
+  { when: "09/10/25", title: "Applications Open!", blurb: "Start working on your application!", cta: "More Info Coming Soon!"},
   { when: "09/13/25", title: "Club Fest", blurb: "Stop by our table at Club Fest to meet more of our current members. They can give you insight about the club!", cta: "See Details" },
   { when: "09/15/25", title: "Academics of Entrepreneurship Panel", blurb: "Come learn more about entrepreneurship by hearing from our Cornell entrepreneurial faculty!", cta: "More Info Coming Soon!"},
   { when: "09/17/25", title: "Info Session #2", blurb: "Come to this information ENT event to learn more about what we have to offer!", cta: "See Details" },
